2c0b2b3bf2da2cd0baeb787c62740f9c2ccf1ed4
Joana Marques <joana.pipaa@gmail.com>
2018-03-15 17:55:23 +0000

    Add a program to roll dices

---
+++ TestRandom/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TestRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string temp;
+            int dados;
+            int soma = 0;
+
+            Random r = new Random();
+
+            Console.Write("Insira o número de dados: ");
+            temp = Console.ReadLine();
+            dados = Convert.ToInt32(temp);
+
+            for (int i = 1; i <= dados; i++)
+            {
+                soma += r.Next(1, 7);
+
+            }
+            Console.WriteLine(soma);
+
+
+             
+       }
+    }
+}
